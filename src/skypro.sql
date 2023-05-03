49ab9ed2733fc79cc8d9cce855fd74ac15102a37
Zavarka47 <pazuzu1995@gmail.com>
2023-05-03 19:43:53 +0300

    Done

--- src/skypro.sql
+++ src/skypro.sql
@@ -86,4 +86,143 @@ skypro=# SELECT * FROM employee;
 (2 строки)
 
 
+    ***********************************
+    Выборка данных  и вложенные запросы
+
 skypro=#
+
+
+skypro=# INSERT INTO employee (
+skypro(# first_name, last_name, gender, age)
+skypro-# VALUES ('Ivan', 'Ivanov', 'm', 17);
+INSERT 0 1
+skypro=# INSERT INTO employee (
+skypro(# first_name, last_name, gender, age)
+skypro-# VALUES ('Ivan', 'Ivanov', 'm', 17);
+INSERT 0 1
+skypro=# INSERT INTO employee (
+skypro(# first_name, last_name, gender, age)
+skypro-# VALUES ('Daria', 'Zhelyaeva', 'w', 27);
+INSERT 0 1
+skypro=# SELECT * FROM employee;
+ id | first_name | last_name | gender | age
+----+------------+-----------+--------+-----
+  3 | Klim       | Chugunkin | m      |  25
+  2 | Daenerys   | Targaryen | w      |  14
+  4 | Ivan       | Ivanov    | m      |  17
+  5 | Ivan       | Ivanov    | m      |  17
+  6 | Daria      | Zhelyaeva | w      |  27
+(5 строк)
+
+skypro=# update employee SET first_name='Peter', last_name='Petrov', gender='m', age='19' where id='4';
+UPDATE 1
+skypro=# select * from employee order by id;
+ id | first_name | last_name | gender | age
+----+------------+-----------+--------+-----
+  2 | Daenerys   | Targaryen | w      |  14
+  3 | Klim       | Chugunkin | m      |  25
+  4 | Peter      | Petrov    | m      |  19
+  5 | Ivan       | Ivanov    | m      |  17
+  6 | Daria      | Zhelyaeva | w      |  27
+(5 строк)
+
+skypro=# SELECT first_name AS Имя, last_name AS Фамилия From employee;
+   Имя    |  Фамилия
+----------+-----------
+ Klim     | Chugunkin
+ Daenerys | Targaryen
+ Ivan     | Ivanov
+ Daria    | Zhelyaeva
+ Peter    | Petrov
+(5 строк)
+                                                             ^
+skypro=# SELECT first_name AS Имя, last_name AS Фамилия From employee where age between 30 and 50;
+ Имя | Фамилия
+-----+---------
+(0 строк)
+
+skypro=# SELECT first_name AS Имя, last_name AS Фамилия From employee where age < 30 or age > 50;
+   Имя    |  Фамилия
+----------+-----------
+ Klim     | Chugunkin
+ Daenerys | Targaryen
+ Ivan     | Ivanov
+ Daria    | Zhelyaeva
+ Peter    | Petrov
+(5 строк)
+
+skypro=# SELECT * From employee order by last_name desc;
+ id | first_name | last_name | gender | age
+----+------------+-----------+--------+-----
+  6 | Daria      | Zhelyaeva | w      |  27
+  2 | Daenerys   | Targaryen | w      |  14
+  4 | Peter      | Petrov    | m      |  19
+  5 | Ivan       | Ivanov    | m      |  17
+  3 | Klim       | Chugunkin | m      |  25
+(5 строк)
+
+skypro=# SELECT first_name From employee where first_name like '____%';
+ first_name
+------------
+ Klim
+ Daenerys
+ Ivan
+ Daria
+ Peter
+(5 строк)
+
+
+skypro=# SELECT first_name From employee where first_name like '_____%';
+ first_name
+------------
+ Daenerys
+ Daria
+ Peter
+(3 строки)
+
+    *******************
+
+
+skypro=# UPDATE employee SET first_name='Ivan', last_name='Ivanov' where id='2';
+UPDATE 1
+skypro=# UPDATE employee SET first_name='Peter', last_name='Petrov' where id='3';
+UPDATE 1
+skypro=# UPDATE employee SET gender='m' where id='2';
+UPDATE 1
+skypro=# Select * from employee order by first_name;
+ id | first_name | last_name | gender | age
+----+------------+-----------+--------+-----
+  6 | Daria      | Zhelyaeva | w      |  27
+  5 | Ivan       | Ivanov    | m      |  17
+  2 | Ivan       | Ivanov    | m      |  14
+  4 | Peter      | Petrov    | m      |  19
+  3 | Peter      | Petrov    | m      |  25
+(5 строк)
+
+skypro=# select first_name as имя, sum(age) as суммарный_возраст from employee group by first_name;
+  имя  | суммарный_возраст
+-------+-------------------
+ Daria |                27
+ Ivan  |                31
+ Peter |                44
+(3 строки)
+
+
+skypro=# select first_name as имя, last_name as фамилия, age as возраст from employee where age=(select min(age) from employee);
+ имя  | фамилия | возраст
+------+---------+---------
+ Ivan | Ivanov  |      14
+(1 строка)
+
+
+
+skypro=# select first_name as имя, last_name as фамилия, age as возраст from employee
+skypro-# where age=(select max(age) from employee where first_name='Ivan')
+skypro-# or
+skypro-# age=(select max(age) from employee where first_name='Peter')
+skypro-# order by age;
+  имя  | фамилия | возраст
+-------+---------+---------
+ Ivan  | Ivanov  |      17
+ Peter | Petrov  |      25
+(2 строки)
